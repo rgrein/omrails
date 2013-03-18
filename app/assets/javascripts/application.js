3784729f3e57078d45c66b1f003bd01b637289d7
Rachelle Grein <rgrein@gmail.com>
2013-03-18 00:54:01 -0400

    Add bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= reuire bootstrap
 //= require_tree .
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Omrails::Application.routes.draw do
+  get 'about' => 'pages#About'
+  root :to => 'pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
